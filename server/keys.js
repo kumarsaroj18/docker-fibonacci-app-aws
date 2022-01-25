81813602f20b826b0ce665d067557bd92ff63a7c
Saroj Kumar <kumarsaroj18@gmail.com>
2022-01-25 21:47:04 +0530

    Incomplete, migrated db from postgres to mysql

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT
+  mySqlUser: process.env.MYSQL_USER,
+  mySqlHost: process.env.MYSQL_HOST,
+  mySqlDatabase: process.env.MYSQL_DATABASE,
+  mySqlPassword: process.env.MYSQL_PASSWORD,
+  mySqlPort: process.env.MYSQL_PORT
 };
